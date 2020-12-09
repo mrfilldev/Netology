694edf2cf8697f75d35001e3a19cf3a605861875
Vladislav Filippov <mr.filldev@gmail.com>
2020-12-10 02:35:21 +0300

    Создал Альфа версию по задаче 2 в дз(бета версия оставлена - там более сложный алгоритм)

---
+++ KotlinDeveloping/2nd Hw/2.2_alpha_version/src/main/kotlin/main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val likes = 111111
+
+    if ((likes % 10 == 1) && (likes != 11)) {
+
+        println("Понравилось $likes человеку ")
+
+    } else{
+
+        println("Понравилось $likes людям ")
+
+    }
+}
